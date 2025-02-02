5ad5adf75a3bfb5e3aab84fe198d03b003110c33
Jaunger <121428460+Jaunger@users.noreply.github.com>
2025-02-02 14:37:53 +0200

    updated app to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    //implementation(project(":AchievementsLibrary"))
    implementation (libs.achievementsdk)
 
 }
